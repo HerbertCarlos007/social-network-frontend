b86a94dc6cd344292cbb4b979315db58d1773975
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2025-08-18 21:16:26 -0300

    feat: photo component

---
+++ src/components/PhotoProfileComponent.vue
@@ -0,0 +1,47 @@
+<script setup></script>
+
+<template>
+  <div class="w-[310px] bg-[#2e3033] ml-20 p-4 rounded-lg shadow-lg mt-5">
+    <div class="flex justify-between items-center">
+      <span class="text-white font-semibold">Fotos</span>
+      <span class="text-blue-400 text-sm cursor-pointer hover:underline"
+        >Ver todas as fotos</span
+      >
+    </div>
+
+    <div class="grid grid-cols-3 gap-1 mt-3">
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+      <img
+        src="https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg"
+        class="aspect-square object-cover rounded-sm w-full h-full"
+        alt="Leão na natureza"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
